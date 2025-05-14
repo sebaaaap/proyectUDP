3269c351e97f47b0832a262a3867e9b5e254c3e4
Dmena1 <dierim11@gmail.com>
2025-05-14 00:06:05 -0400

    Hola negros

--- proyecto/frontend/src/Paginas/Home.jsx
+++ proyecto/frontend/src/Paginas/Home.jsx
@@ -1,7 +1,7 @@
 
 export const Home = () => {
     return (
-        <h1>Soy un casa miren soy la casa holaaa soy el seba
+        <h1>Soy un casa miren soy la casa holaaa soy el seba pe
         </h1>
     )
 
