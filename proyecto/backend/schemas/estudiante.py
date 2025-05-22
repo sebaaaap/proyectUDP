377715fd4135ce699addf1d1f7d23a58641e7642
harol <harol.bustos@mail.udp.cl>
2025-05-22 11:21:26 -0400

    backend: cambios login, registro y modelos estudiante

--- proyecto/backend/schemas/estudiante.py
+++ proyecto/backend/schemas/estudiante.py
@@ -1,6 +1,7 @@
 #schemas
 from pydantic import BaseModel
 from datetime import date
+from typing import Optional, Dict, Any
 
 class EstudianteBase(BaseModel):
     rut_estudiante: str
@@ -12,9 +13,29 @@ class EstudianteBase(BaseModel):
     genero: str | None = None
     anio_academico_ingreso: int | None = None
     periodo_academico_ingreso: int | None = None
+    email: EmailStr
 
 class EstudianteOut(EstudianteBase):
     id_st: int
 
     class Config:
         orm_mode = True
+
+class EstudianteCreate(EstudianteBase):
+    password: str
+
+class EstudianteResponse(EstudianteBase):
+    id: int
+
+    class Config:
+        from_attributes = True
+
+class EstudianteLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class LoginResponse(BaseModel):
+    access_token: str
+    token_type: str
+    estudiante: EstudianteResponse
