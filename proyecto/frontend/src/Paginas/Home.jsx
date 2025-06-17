cf7413dc92a755cca1c37f47cb71c8ff2c265285
diegoclemente0 <diegoxclem12@gmail.com>
2025-06-17 15:25:36 -0400

    mensajito lindo

--- proyecto/frontend/src/Paginas/Home.jsx
+++ proyecto/frontend/src/Paginas/Home.jsx
@@ -1,7 +1,7 @@
 // Home.jsx
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useAuth } from '../../context/AuthContext';
+import { useAuth } from '../context/AuthContext.jsx';
 
 export const Home = () => {
     const { isAuthenticated } = useAuth();
--- proyecto/frontend/src/Paginas/Login.jsx
+++ proyecto/frontend/src/Paginas/Login.jsx
@@ -2,7 +2,7 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 // src/Paginas/Login.jsx
-import { useAuth } from '../../context/AuthContext';
+import { useAuth } from '../context/AuthContext';
 
 export function Login() {
     const { isAuthenticated, login } = useAuth();
